f5b0227d42a42ef4a562411aef6d133bb4db6cf5
zamzamat <azamat.atikeev@gmail.com>
2025-08-28 19:30:42 +0500

    Created Image from the 1st Task

--- hw1/A.py
+++ hw1/A.py
@@ -5,7 +5,7 @@ import imageio.v2 as imageio
     # 1 (15%) A grayscale image of constant intensity 104
     # 2 (15%) A grayscale image with alternating black and white vertical stripes, each of which is 4 pixels wide 
     # 3 (15%) A grayscale image with a ramp intensity distribution, described by I(x, y) = x/2
-    # 4 (15%) A grayscale image with a Gaussian intensity distribution centered at (128, 128), described by [formula]
+    # 5 (15%) A grayscale image with a Gaussian intensity distribution centered at (128, 128), described by [formula]
     # 5 (15%) A color image where the upper left quadrant is yellow, the lower left quadrant is red, the upper right quadrant is green, and the lower right quadrant is black
     # 6 (15%) For questions 1-5, create a document consisting of your inputs, outputs, your observations, problems you have faced, solutions indicating how you have overcome the problems, and other points that you think are necessary.
     # 7 (10%) For questions 1-5, create a video describing your solutions
@@ -24,3 +24,17 @@ Matrix = np.full(   (height, width), #matrix dimensions
 
 #And finally we a special library to create an imgae from a Matrix data
 imageio.imwrite("1_Gray_104.png", Matrix)
+
+
+    ###2
+#We already created an Image for the 1st task before, and we can still use the data from previous task
+#We will use the same MAtrix, but replace numbers in it
+
+for x in range(width): #here we take every column
+    if (x // 4) % 2 == 0: #divide column number by 4 without remainder, and check if it is odd or even
+        Matrix[:, x] = 0 #Paint column X in Black. 0 Min intensity in 8-bit
+    else:
+        Matrix[:, x] = 255 #Paint column in White. 255 Max intensity in 8-bit
+
+#After rewriting data in Matrix, We create image:
+imageio.imwrite("2_Stripes_BW.png", Matrix)
